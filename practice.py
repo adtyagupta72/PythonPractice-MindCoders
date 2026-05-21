e7e598f46da6c72a7d92f916ab495d708b778642
Adtya Gupta <adtyagupta72@gmail.com>
2026-05-21 14:12:54 +0530

    Python: List methods

--- practice.py
+++ practice.py
@@ -17,6 +17,48 @@ print("Testing commit changes")
 # print(my_list)
 # print(count)
 
-my_list = [8, 10, 6, 2, 4]
-my_list.sort()
-print(my_list)
+# my_list = [8, 10, 6, 2, 4]
+# my_list.sort()
+# print(my_list)
+# my_list.reverse()
+# print(my_list)
+
+# list_1 = [1]
+# list_2 = list_1[:]
+# list_1[0] = 2
+# print(list_2)
+# print(list_1)
+
+# my_list = [10, 8, 6, 4, 2]
+# new_list = my_list[1:3]
+# print(new_list)
+# my_list = [10, 8, 6, 4, 2]
+# new_list = my_list[1:-1]
+# print(new_list)
+
+
+# my_list = [10, 8, 6, 4, 2]
+# new_list = my_list[-1:1]
+# new_list = my_list[-5:3]
+# new_list = my_list[:3]
+# print(new_list)
+# new_list = my_list[2:]
+# print(new_list)
+#del my_list[1:3]
+
+#del my_list
+
+# del my_list[:]
+# print(my_list)
+
+# my_list = [0, 3, 12, 8, 2]
+# print(5 in my_list)
+# print(5 not in my_list)
+# print(12 in my_list)
+
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+del list_1[0]
+del list_2
+print(list_3)
