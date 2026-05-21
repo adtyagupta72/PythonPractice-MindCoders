758faf4df1dae4fdcc17ffe4021ad3459a81faee
Adtya Gupta <adtyagupta72@gmail.com>
2026-05-21 18:06:52 +0530

    Python: List Compression completed

--- practice.py
+++ practice.py
@@ -79,41 +79,86 @@ print("Testing commit changes")
 # odds = [f'{element} Is Odd Number!' for element in squares if element % 2 != 0 ]
 # print(odds)
 
-board = []
-for i in range(8):
-    row = ["EMPTY" for i in range(8)]
-    board.append(row)
+# board = []
+# for i in range(8):
+#     row = ["EMPTY" for i in range(8)]
+#     board.append(row)
 
-#print(board)
-for element in board:
-    print(element)
+# #print(board)
+# for element in board:
+#     print(element)
 
-# print(len(board))
+# # print(len(board))
 
-#print(board[0][0])
+# #print(board[0][0])
 
-board[0][0] = "ROOK"
-board[0][7] = "ROOK"
-board[7][0] = "ROOK"
-board[7][7] = "ROOK"
+# board[0][0] = "ROOK"
+# board[0][7] = "ROOK"
+# board[7][0] = "ROOK"
+# board[7][7] = "ROOK"
 
-print("---------------")
+# print("---------------")
 
-for element in board:
-    print(element)
+# for element in board:
+#     print(element)
 
-board[0][1] = "KNIGHT"
-board[0][6] = "KNIGHT"
-board[7][1] = "KNIGHT"
-board[7][6] = "KNIGHT"
+# board[0][1] = "KNIGHT"
+# board[0][6] = "KNIGHT"
+# board[7][1] = "KNIGHT"
+# board[7][6] = "KNIGHT"
 
-print("---------------")
+# print("---------------")
 
-for element in board:
-    print(element)
+# for element in board:
+#     print(element)
 
 
-temps = [[0.0 for h in range(24)] for d in range(31)]
-for element in temps:
-    print(element)
+# temps = [[0.0 for h in range(24)] for d in range(31)]
 
+# temp1 = 19
+# temp2 = 32
+# count = 0
+
+# for days in temps:
+#     if count == 0:
+#         days[11] = temp1
+#         count = 1
+#     else:
+#         days[11] = temp2
+#         count = 0
+    
+# for element in temps:
+#     print(element)
+
+# total = 0.0
+# for day in temps:
+#     total += day[11]
+# average = total / 31
+# print("Average temperature at noon:", average)
+
+# highest = -100.0
+# for day in temps:
+#     for temp in day:
+#         if temp > highest:
+#             highest = temp
+# print("The highest temperature was:", highest)
+
+# hot_days = 0
+# for day in temps:
+#     if day[11] > 20.0:
+#         hot_days += 1
+# print(hot_days, "days were hot days in the month.")
+
+rooms = [[[False for r in range(20)] for f in range(15)] for t in range(3)]
+
+print(rooms)
+
+rooms[1][9][13] = True
+
+rooms[1][9][1] = True
+
+vacancy = 0
+for room_number in range(20):
+    if not rooms[1][9][room_number]:
+        vacancy += 1
+print("Vacancy in 3rd 15th floor of 3rd Building", vacancy)
