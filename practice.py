345d8e76dc14a2494eeabcef35d17850a4ce1448
Adtya Gupta <adtyagupta72@gmail.com>
2026-05-25 18:17:47 +0530

    Python: Functions

--- practice.py
+++ practice.py
@@ -300,10 +300,46 @@ v (3)           0      1    2
 
 # happy_new_year(False)
 
-def boring_function():
-    print("'Boredom Mode' ON.")
-    return 123
+# def boring_function():
+#     print("'Boredom Mode' ON.")
+#     return 123
 
-print("This lesson is interesting!")
-boring_function()
-print("This lesson is boring...")
+# print("This lesson is interesting!")
+# boring_function()
+# print("This lesson is boring...")
+
+# print(None + 2)
+
+# def checkMyVar(variable):
+#     if(variable == 10):
+#         print("Variable is 10")
+#         return 2
+#     else:
+#         print("Variable is not up to the mark")
+#         return
+    
+# checkMyVar(5)
+# print()
+
+
+# def list_sum(lst):
+#     s = 0
+ 
+#     for elem in lst:
+#         s += elem
+ 
+#     return s
+# print(list_sum([5, 4, 3]))
+# print(list_sum(2))
+
+
+def strange_list_fun(n):
+    strange_list = []
+    
+    for i in range(0, n):
+        #strange_list.insert(0, i+1)
+        strange_list.append(i+1)
+    
+    return strange_list
+
+print(strange_list_fun(5))
