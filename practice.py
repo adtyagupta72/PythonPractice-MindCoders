773e9793f081536c6b5c76a392e35651aec6aad2
Adtya Gupta <adtyagupta72@gmail.com>
2026-06-08 16:44:44 +0530

    Python: OOPs WIP

--- practice.py
+++ practice.py
@@ -667,10 +667,34 @@ op  5   4   3   2   1   0
 # obj.var = 3
 # obj.method()
 
-class Star:
-    def __init__(self, name, galaxy):
-        self.name = name
-        self.galaxy = galaxy
+# class Star:
+#     def __init__(self, name, galaxy):
+#         self.name = name
+#         self.galaxy = galaxy
 
-sun = Star("Sun", "Milky Way")
-print(sun)
+# sun = Star("Sun", "Milky Way")
+# print(sun)
+
+class ExampleClass:
+    counter = 0
+    def __init__(self, val = 1):
+        #self.__first = val
+        ExampleClass.counter += 1
+        if val % 2 != 0:
+            self.a = 1
+        else:
+            self.b = 1
+
+example_object = ExampleClass(6)
+print(example_object.a)
+print(example_object.b)
+
+
+# example_object_1 = ExampleClass()
+# example_object_2 = ExampleClass(2)
+# example_object_3 = ExampleClass(4)
+ 
+# print(example_object_1.__dict__, example_object_1.counter)
+# print(example_object_2.__dict__, example_object_2.counter)
+# print(example_object_3.__dict__, example_object_3.counter)
+
