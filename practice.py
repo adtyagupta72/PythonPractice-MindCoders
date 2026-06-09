80e86c1d91a24af9d736bc1ead3b7628e9763473
Adtya Gupta <adtyagupta72@gmail.com>
2026-06-09 16:38:03 +0530

    Python: OOPs Inheretance

--- practice.py
+++ practice.py
@@ -768,22 +768,53 @@ op  5   4   3   2   1   0
 #     print()
 
 
-class SampleClass:
-    def __init__(self, val):
-        self.val = val
+# class SampleClass:
+#     def __init__(self, val):
+#         self.val = val
 
-object_1 = SampleClass(0)
-object_2 = SampleClass(2)
-object_3 = object_1
-object_3.val += 1
+# object_1 = SampleClass(0)
+# object_2 = SampleClass(2)
+# object_3 = object_1
+# object_3.val += 1
 
-print(object_1 is object_2)
-print(object_2 is object_3)
-print(object_3 is object_1)
-print(object_1.val, object_2.val, object_3.val)
+# print(object_1 is object_2)
+# print(object_2 is object_3)
+# print(object_3 is object_1)
+# print(object_1.val, object_2.val, object_3.val)
 
-string_1 = "Mary had a little "
-string_2 = "Mary had a little lamb"
-string_1 += "lamb"
+# string_1 = "Mary had a little "
+# string_2 = "Mary had a little lamb"
+# string_1 += "lamb"
 
-print(string_1 == string_2, string_1 is string_2)
+# print(string_1 == string_2, string_1 is string_2)
+
+# class Super:
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def __str__(self):
+#         return "My name is " + self.name + "."
+
+# class Sub(Super):
+#     def __init__(self, name):
+#         super().__init__(name)
+
+# obj = Sub("Andy")
+# print(obj)
+
+class SuperA:
+    var_a = 10
+    def fun_a(self):
+        return 11
+
+class SuperB:
+    var_b = 20
+    def fun_b(self):
+        return 21
+
+class Sub(SuperA, SuperB):
+    pass
+
+obj = Sub()
+print(obj.var_a, obj.fun_a())
+print(obj.var_b, obj.fun_b())
